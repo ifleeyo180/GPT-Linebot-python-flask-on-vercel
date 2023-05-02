ec9b72233d33705809b8bf814d1528dee693a875
iFleeyo180 <97100439+ifleeyo180@users.noreply.github.com>
2023-05-03 05:20:34 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, abort
-from flask_sqlalchemy import SQLAlchemy
 from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from api.chatgpt import ChatGPT
-from apscheduler.schedulers.background import BackgroundScheduler
+
 import os
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
@@ -13,16 +12,7 @@ working_status = os.getenv(
     "DEFALUT_TALKING", default="flase").lower() == "true"
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
 chatgpt = ChatGPT()
-
-
-class Subscriber(db.Model):
-    id = db.Column(db.String(50), primary_key=True)
-
-
-db.create_all()
 
 # domain root
 
@@ -49,8 +39,8 @@ def callback():
 
 # 定義關鍵字字典
 keywords = {
-    "/指令查詢": "目前指令有：\n ✏ /日報 \n ✏ /erp \n ✏ /信箱 \n ✏ /說話 (開啟機器人對話) \n ✏ /安靜 (關閉機器人對話) \n👉 記得加 / ❗",
-    "/日報": "https://reurl.cc/Y86yq4",
+    "/指令查詢": "目前指令有：\n ✏ /日誌 \n ✏ /erp \n ✏ /信箱 \n ✏ /說話 (開啟機器人對話) \n ✏ /安靜 (關閉機器人對話) \n👉 記得加 / ❗",
+    "/日誌": "https://reurl.cc/Y86yq4",
     "/erp": "https://reurl.cc/d756yq",
     "/信箱": "https://reurl.cc/6Nlrdk",
 }
@@ -60,37 +50,6 @@ keywords = {
 def handle_message(event):
     global working_status
     if event.message.type != "text":
-        return
-
-    if event.message.text == "/訂閱":
-        # 增加訂閱者
-        subscriber = Subscriber.query.get(event.source.user_id)
-        if subscriber is None:
-            new_subscriber = Subscriber(id=event.source.user_id)
-            db.session.add(new_subscriber)
-            db.session.commit()
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="您已成功訂閱！"))
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="您已經訂閱。"))
-        return
-
-    if event.message.text == "/取消訂閱":
-        # 移除訂閱者
-        subscriber = Subscriber.query.get(event.source.user_id)
-        if subscriber is not None:
-            db.session.delete(subscriber)
-            db.session.commit()
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="您已取消訂閱每週日誌提醒。"))
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="您尚未訂閱。"))
         return
 
     if event.message.text == "/說話":
@@ -123,23 +82,5 @@ def handle_message(event):
             TextSendMessage(text=reply_msg))
 
 
-def send_reminder():
-    subscribers = Subscriber.query.all()
-    for subscriber in subscribers:
-        try:
-            line_bot_api.push_message(subscriber.id, TextSendMessage(
-                text="請記得填寫日報，連結如下：\nhttps://reurl.cc/Y86yq4"))
-        except LineBotApiError as e:
-            print(f"無法發送訊息給 {subscriber.id}: {e}")
-            # 移除已封鎖機器人或已刪除帳戶的用戶
-            db.session.delete(subscriber)
-            db.session.commit()
-
-
 if __name__ == "__main__":
-    scheduler = BackgroundScheduler()
-    # scheduler.add_job(send_reminder, 'cron', day_of_week='sun', hour=9)
-    scheduler.add_job(send_reminder, 'interval', seconds=30)
-    scheduler.start()
-
-    app.run(debug=True)
+    app.run()
