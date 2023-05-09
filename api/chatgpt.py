818627744ef1e30b0e05cd14727c39d4c54d2e74
ifleeyo180 <ifleeyo180@gmail.com>
2023-05-10 03:42:06 +0800

    fix

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -9,8 +9,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default="gpt-3.5-turbo")
-        # self.model = os.getenv("OPENAI_MODEL", default="text-davinci-003")
+        self.model = os.getenv("OPENAI_MODEL", default="text-davinci-003")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default=0))
         self.frequency_penalty = float(
             os.getenv("OPENAI_FREQUENCY_PENALTY", default=0))
@@ -19,15 +18,15 @@ class ChatGPT:
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=240))
 
     def get_response(self):
-        response = openai.ChatCompletion.create(
+        response = openai.Completion.create(
             model=self.model,
-            messages=self.prompt.messages,
+            prompt=self.prompt.generate_prompt(),
             temperature=self.temperature,
             frequency_penalty=self.frequency_penalty,
             presence_penalty=self.presence_penalty,
             max_tokens=self.max_tokens
         )
-        return response['choices'][0].text.strip()
+        return response['choices'][0]['text'].strip()
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
